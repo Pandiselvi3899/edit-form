44eaf53671b67763eee8ec287abe9ce6d53cf8c2
Pandi Selvi <89200308+Pandiselvi3899@users.noreply.github.com>
2021-11-10 16:54:02 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Signup } from "./containers/Signup";
+
+function App() {
+  return (
+    <div className="App">
+      <Signup />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App/>, rootElement);
